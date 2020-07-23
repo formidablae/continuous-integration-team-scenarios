61d650aa1c5aa246c6a51ed9b6118c26fd6a988b
Nick Taranov <nicktaranov@gmail.com>
2020-07-22 21:59:06 -0400

    Add first CI steps

--- ci.md
+++ ci.md
@@ -1,2 +1,8 @@
 # GitHub Flow
 
+1. Pull in the latest code. Create a branch from `master`. Start working.    
+2. Create commits on your new branch. Build and test locally.  
+  Pass? Go to the next step. Fail? Fix errors or tests and try again.  
+3. Push to your remote repository or remote branch.  
+4. Open a pull request. Discuss the changes, add more commits  
+  as discussion continues. Make tests pass on the feature branch.  
--- ci.test.js
+++ ci.test.js
@@ -12,7 +12,21 @@ describe('CI sequence', () => {
   });
 
   // TODO add the tests between these comments =>
+  it('1. pull latest code', () => {
+    expect(/.*pull.*/ig.test(fileContents)).toBe(true);
+  });
 
+  it('2. add commits', () => {
+    expect(/.*commit.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('3. push to the remote branch with the same name', () => {
+    expect(/.*push.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('4. open a pull request and continue working', () => {
+    expect(/.*pull\s+request.*/ig.test(fileContents)).toBe(true);
+  });
   // TODO <= add the tests between these comments
 
 });
